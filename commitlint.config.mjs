23ad7784939fd784a59ea9db5730e9db5748c497
Steven Hall <Steven.hall@goldenhippo.com>
2025-09-22 14:49:20 -0500

    chore: configure commitlint

---
+++ commitlint.config.mjs
@@ -0,0 +1,12 @@
+const Configuration = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'docs', 'chore', 'style', 'refactor', 'ci', 'test', 'perf', 'revert',],
+    ],
+  },
+}
+
+export default Configuration
